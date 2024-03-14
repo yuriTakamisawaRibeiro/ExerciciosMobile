828fee2692103cfcccb175e005e4edac8c027133
yuriTakamisawaRibeiro <yuri.takamisawa.ribeiro@gmail.com>
2024-03-14 10:54:41 -0300

    exercicio2

--- app/src/main/java/com/fatec/mobile/Exercicio1.java
+++ app/src/main/java/com/fatec/mobile/Exercicio1.java
@@ -1,7 +1,13 @@
 package com.fatec.mobile;
 
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import com.fatec.mobile.R;
 
 public class Exercicio1 extends AppCompatActivity {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exercicio1layout);
+    }
 }
---
+++ app/src/main/java/com/fatec/mobile/Exercicio2.java
@@ -0,0 +1,5 @@
+package com.fatec.mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class Exercicio2 extends AppCompatActivity{
+}
