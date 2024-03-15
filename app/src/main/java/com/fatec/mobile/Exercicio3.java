3b37db972d62e8e8dea48aa9c28fde94933ba4d1
yuriTakamisawaRibeiro <yuri.takamisawa.ribeiro@gmail.com>
2024-03-15 09:35:02 -0300

    Exercicio 3

---
+++ app/src/main/java/com/fatec/mobile/Exercicio3.java
@@ -0,0 +1,14 @@
+package com.fatec.mobile;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Exercicio3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exercicio3layout);
+    }
+}
